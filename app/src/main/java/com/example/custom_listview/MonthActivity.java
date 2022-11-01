58b18341e0b12229b684112f1766c739292993ef
mhaamahdhi1 <2017t00041@stu.cmb.ac.lk>
2022-11-01 23:51:27 +0530

    starter

---
+++ app/src/main/java/com/example/custom_listview/MonthActivity.java
@@ -0,0 +1,41 @@
+package com.example.custom_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.custom_listview.databinding.ActivityMonthBinding;
+
+public class MonthActivity extends AppCompatActivity {
+
+    ActivityMonthBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMonthBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        setTitle("Detail View");
+
+        Intent intent = this.getIntent();
+
+        if (intent != null){
+            String name = intent.getStringExtra("name");
+            String MonthNo = intent.getStringExtra("MonthNo");
+            int imageid = intent.getIntExtra("imageid",R.drawable.pic_1);
+            String time=intent.getStringExtra("time");
+
+            System.out.println(time);
+
+            binding.nameProfile.setText(name);
+            binding.monthNo.setText(MonthNo);
+            binding.profileImage.setImageResource(imageid);
+            binding.time.setText(time);
+
+
+        }
+
+    }
+}
